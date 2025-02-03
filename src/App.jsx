b4ded3a05f5e3ea8a4f0b8bdac75c0cdd360dcfd
Ankit-coder7870 <ankitanand1774@gmail.com>
2025-02-03 17:01:42 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+
+import "./App.css"
+
+import { AppLayout } from "./components/Layout/AppLayout";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { ErrorPage } from "./pages/ErrorPage";
+import { Home } from "./pages/Home";
+import { Country } from "./pages/Country";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CountryDetails } from "./components/Layout/CountryDetails";
+
+
+const router = createBrowserRouter([{
+
+  path: "/",
+  element: <AppLayout />,
+  errorElement: <ErrorPage />,
+  children: [
+
+    {
+      path: "/",
+      element: <Home />,
+
+    },
+    {
+      path: "about",
+      element: <About />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
+    },
+    {
+      path: "country",
+      element: <Country />,
+    },
+    {
+      path: "country/:id",
+      element: <CountryDetails />,
+    },
+  ],
+
+
+
+
+
+},])
+const App = () => {
+  return <RouterProvider router={router}> </RouterProvider>;
+
+
+
+}
+
+export default App;
